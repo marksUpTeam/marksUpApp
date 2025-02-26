107851e94691b2b0d9aae7ec1af26b1d2bd730e8
Me <alint@bk.ru>
2025-02-26 12:26:09 +0300

    added auth models

---
+++ app/src/main/java/ru/bmstu/marksUpTeam/android/marksUpApp/data/AuthorizationModels.kt
@@ -0,0 +1,65 @@
+package ru.bmstu.marksUpTeam.android.marksUpApp.data
+
+import kotlinx.serialization.Serializable
+
+// PROFILE MODELS
+
+@Serializable
+data class Person(
+    val id: Long,
+    val name: String,
+    val surname: String,
+    val email: String,
+    val phone: String,
+)
+
+@Serializable
+data class Discipline(
+    val id: Long,
+    val name: String,
+    val complexity: String
+)
+
+@Serializable
+data class Teacher(
+    val id: Long,
+    val person: Person,
+    val disciplines: List<Discipline>
+)
+
+@Serializable
+data class Child(
+    val id: Long,
+    val person: Person,
+)
+
+@Serializable
+data class Parent(
+    val id: Long,
+    val person: Person,
+    val children: List<Child>
+
+)
+
+@Serializable
+data class Student(
+    val id: Long,
+    val child: Child,
+    val parent: Parent,
+)
+
+
+@Serializable
+data class Profile(
+    val id: Long,
+    val username: String,
+    val parent: Parent?,
+    val student: Student?,
+    val teacher: Teacher?,
+) {
+    init {
+        if (student == null && teacher == null && parent == null) {
+            throw IllegalArgumentException("At least one should be non-null!")
+        }
+    }
+}
