9b0dcb9889b3002e25e1c3035652194ec846330d
naburnm8 <alint@bk.ru>
2025-02-28 20:48:59 +0300

    added auth activity

---
+++ app/src/main/java/ru/bmstu/marksUpTeam/android/marksUpApp/AuthorizationActivity.kt
@@ -0,0 +1,16 @@
+package ru.bmstu.marksUpTeam.android.marksUpApp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import ru.bmstu.marksUpTeam.android.marksUpApp.ui.Authorization
+
+class AuthorizationActivity: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Authorization()
+        }
+    }
+}
+
