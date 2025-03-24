588e393c03a0d10ad624508c295d9cb52266bc16
naburnm8 <alint@bk.ru>
2025-03-25 01:37:00 +0300

    cleaned AuthorizationActivity.kt

--- app/src/main/java/ru/bmstu/marksUpTeam/android/marksUpApp/AuthorizationActivity.kt
+++ app/src/main/java/ru/bmstu/marksUpTeam/android/marksUpApp/AuthorizationActivity.kt
@@ -22,56 +22,17 @@ import ru.bmstu.marksUpTeam.android.marksUpApp.ui.authorization.AccountNotFoundS
 import ru.bmstu.marksUpTeam.android.marksUpApp.ui.authorization.Authorization
 import ru.bmstu.marksUpTeam.android.marksUpApp.ui.ErrorScreen
 import ru.bmstu.marksUpTeam.android.marksUpApp.ui.LoadingScreen
+import ru.bmstu.marksUpTeam.android.marksUpApp.ui.authorization.AuthorizationScreen
+import ru.bmstu.marksUpTeam.android.marksUpApp.ui.authorization.AuthorizationViewModel
 import ru.bmstu.marksUpTeam.android.marksUpApp.ui.theme.MarksUpTheme
 
 class AuthorizationActivity: ComponentActivity() {
-    private val authorizationRepository by lazy { AuthorizationRepository(BuildConfig.API_URL, this) }
+    private val authorizationViewModel: AuthorizationViewModel by lazy { AuthorizationViewModel(BuildConfig.API_URL, this) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         println(BuildConfig.API_URL)
         setContent {
-            MarksUpTheme(){
-            val coroutineScope = rememberCoroutineScope()
-            var didFail by rememberSaveable { mutableStateOf(false) }
-            var loading by rememberSaveable { mutableStateOf(false) }
-            var error by rememberSaveable { mutableStateOf(false) }
-            var profileExists by rememberSaveable { mutableStateOf(0) }
-            var errorMsg by rememberSaveable { mutableStateOf("") }
-            if (profileExists == 1) {
-                val prefs = getEncryptedSharedPreferences(this)
-                prefs.edit().putBoolean("is_logged_in", true).apply()
-                finish()
-            }
-            when {
-                profileExists == 2 -> {
-                    AccountNotFoundScreen(onPress = {profileExists = 0;}, tint = MaterialTheme.colorScheme.secondary,
-                        containerColor = MaterialTheme.colorScheme.onBackground,
-                        backgroundColor = MaterialTheme.colorScheme.background)
-                }
-                error -> {
-                    ErrorScreen(onRefresh = {
-                        error = false
-                    }, modifier = Modifier.fillMaxSize(), errorMessage = errorMsg, backgroundColor = MaterialTheme.colorScheme.background, tint = MaterialTheme.colorScheme.secondary)
-                }
-                loading -> {
-                    LoadingScreen(modifier = Modifier.fillMaxSize(), backgroundColor = MaterialTheme.colorScheme.background, tint = MaterialTheme.colorScheme.secondary)
-                }
-               else -> Authorization(
-                    onAuth = { oauth, token -> run {testProfileCall(
-                        coroutineScope,
-                        authorizationRepository,
-                        setResult = {if (it) {
-                            profileExists = 1
-                        } else {
-                            profileExists = 2
-                        } },
-                        setError = {errorMsgThis, stat -> run {error = stat; errorMsg = errorMsgThis}},
-                        setLoading = {loading = it},
-                        jwt = token.idToken ?: "") }; processJWT(token.idToken ?: "", this) },
-                    didFail = didFail,
-                    onFail = { _, authFail -> run { didFail = true; println(authFail) } }, backgroundColor = MaterialTheme.colorScheme.background, iconTint = MaterialTheme.colorScheme.primary)
-            }
-        }
+            AuthorizationScreen(authorizationViewModel, this)
         }
     }
 }
