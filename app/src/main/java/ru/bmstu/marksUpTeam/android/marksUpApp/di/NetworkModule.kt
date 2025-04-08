1e5d834647d0a16a70efcafcbc1ea65acfe336d3
naburnm8 <alint@bk.ru>
2025-04-09 01:10:11 +0300

    added non intercepted retrofit to DI

--- app/src/main/java/ru/bmstu/marksUpTeam/android/marksUpApp/di/NetworkModule.kt
+++ app/src/main/java/ru/bmstu/marksUpTeam/android/marksUpApp/di/NetworkModule.kt
@@ -1,5 +1,6 @@
 package ru.bmstu.marksUpTeam.android.marksUpApp.di
 
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import ru.bmstu.marksUpTeam.android.marksUpApp.BuildConfig
@@ -10,17 +11,21 @@ import ru.bmstu.marksUpTeam.android.marksUpApp.data.network.favourites.Favourite
 import ru.bmstu.marksUpTeam.android.marksUpApp.data.network.profile.ProfileApi
 import ru.bmstu.marksUpTeam.android.marksUpApp.data.network.registration.RegistrationApi
 import ru.bmstu.marksUpTeam.android.marksUpApp.tools.getBasicInterceptedRetrofit
+import ru.bmstu.marksUpTeam.android.marksUpApp.tools.getBasicRetrofit
 import ru.bmstu.marksUpTeam.android.marksUpApp.tools.getJwt
 
 val networkModule = module{
     single {
-        getBasicInterceptedRetrofit(api =  BuildConfig.API_URL,context = get(), jwtUnformatted = getJwt(context = get()).orEmpty())
+        getBasicInterceptedRetrofit(api =  BuildConfig.API_URL, context = get(), jwtUnformatted = getJwt(context = get()).orEmpty())
+    }
+    single(named("nonIntercepted")){
+        getBasicRetrofit(api = BuildConfig.API_URL, context = get())
     }
     single{
         get<Retrofit>().create(AssignmentsApi::class.java)
     }
     single{
-        get<Retrofit>().create(AuthorizationApi::class.java)
+        get<Retrofit>(named("nonIntercepted")).create(AuthorizationApi::class.java)
     }
     single{
         get<Retrofit>().create(ClassesApi::class.java)
