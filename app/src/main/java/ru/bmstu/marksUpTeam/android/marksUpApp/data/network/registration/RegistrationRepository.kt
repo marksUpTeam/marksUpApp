51d809c62b31b7badf9d03a76513f1420c6bd4db
Me <alint@bk.ru>
2025-04-09 12:43:15 +0300

    filled in RegistrationRepository.kt

--- app/src/main/java/ru/bmstu/marksUpTeam/android/marksUpApp/data/network/registration/RegistrationRepository.kt
+++ app/src/main/java/ru/bmstu/marksUpTeam/android/marksUpApp/data/network/registration/RegistrationRepository.kt
@@ -1,7 +1,27 @@
 package ru.bmstu.marksUpTeam.android.marksUpApp.data.network.registration
 
+import ru.bmstu.marksUpTeam.android.marksUpApp.data.network.profile.ProfileMapper
+import ru.bmstu.marksUpTeam.android.marksUpApp.domain.ProfileDomain
+import java.io.IOException
+
 // RegistrationRepository uses ProfileMapper
 
-class RegistrationRepository(val registrationApi: RegistrationApi) {
+data object AccountAlreadyExistsException : Exception() {
+    private fun readResolve(): Any = AccountAlreadyExistsException
+}
 
+
+class RegistrationRepository(val registrationApi: RegistrationApi) {
+    suspend fun register(profile: ProfileDomain): Result<ProfileDomain> {
+        val profileDto = ProfileMapper().toDto(profile)
+        val response = registrationApi.postNewProfile(profileDto)
+        if (response.code() == 400){
+            return Result.failure(AccountAlreadyExistsException)
+        }
+        return if (response.isSuccessful && response.body() != null) {
+            Result.success(ProfileMapper().map(response.body()!!))
+        } else {
+            Result.failure(IOException(response.errorBody()?.string() ?: "Something went wrong"))
+        }
+    }
 }
